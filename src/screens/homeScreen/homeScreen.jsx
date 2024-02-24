408944879ab9ae381acb9db1442db7e856c21cae
maximelebonm <lebon.maxime.m@gmail.com>
2024-02-24 18:12:45 +0100

    ok

--- src/screens/homeScreen/homeScreen.jsx
+++ src/screens/homeScreen/homeScreen.jsx
@@ -2,11 +2,18 @@ import './homeScreen.css'
 
 import { HomeProductCards } from "../../components/cards/homeProductCard/homeProductCard"
 export const HomeScreen = () => {
+    let environement;
+ console.log(process.env.NODE_ENV)
+    if(process.env.NODE_ENV === 'producion'){
+        environement = 'prod'
+    } else {
+        environement = 'dev'
+    }
 
     return (
         <div id='homeScreenContainer'>
         <section>
-
+            {environement}
         </section>
         <section id="homeScreenProducts">
             <HomeProductCards/>
